179ac9e10032c67ee8abedd42daedb60aa81548c
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-08-15 15:12:30 +0700

    Share through Email

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.core.mail import send_mail
+
+from .models import Post
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)

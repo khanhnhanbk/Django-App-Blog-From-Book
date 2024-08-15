72eb7d88c76d45f4c656cfe5b8f71e4a29593678
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-08-15 09:50:27 +0700

    handle paging except

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
@@ -8,8 +8,14 @@ from .models import Post
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
-    page = request.GET.get("page", 1)
-    posts = paginator.get_page(page)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(
         request,
         "blog/post/list.html",

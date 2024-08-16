559ab307f055f9c2cef28586d829ae867770c6e4
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-08-16 08:00:28 +0700

    add tags page and search by tag

--- blog/views.py
+++ blog/views.py
@@ -7,33 +7,40 @@ from django.views.decorators.http import require_POST
 from .forms import CommentForm, EmailPostForm
 
 from .models import Post
+from taggit.models import Tag
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get("page", 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
-#     return render(
-#         request,
-#         "blog/post/list.html",
-#         {
-#             "posts": posts,
-#         },
-#     )
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        "blog/post/list.html",
+        {
+            "posts": posts,
+            "tag": tag,
+        },
+    )
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = "posts"
+#     paginate_by = 3
+#     template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):

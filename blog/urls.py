e370563cc6418bdd1ac17f5dfabc833615a49e09
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-08-16 15:17:00 +0700

    chore: Add RSS feed for latest blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = "blog"
 
@@ -22,5 +23,10 @@ urlpatterns = [
         "<int:post_id>/comment/",
         views.post_comment,
         name="post_comment",
+    ),
+    path(
+        "feed/",
+        LatestPostsFeed(),
+        name="post_feed",
     ),
 ]

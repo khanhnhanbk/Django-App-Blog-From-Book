d4012beb02a46fb46f071da13833d3a1abe0ccb6
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-08-30 09:39:08 +0700

    add share button

--- blog/views.py
+++ blog/views.py
@@ -77,6 +77,7 @@ def post_detail(request, year, month, day, post):
             "comment_form": form,
             "comments": comments,
             "similar_posts": similar_posts,
+            "request": request,  # Add this line
         },
     )
 

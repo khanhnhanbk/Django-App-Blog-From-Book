2ace41589951ad1d7eae786f2f057c96a45baa66
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-09-05 08:57:40 +0700

    change setting to push to docker.

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', os.getenv('DB_NAME')),
-        'USER': os.environ.get('POSTGRES_USER', os.getenv('DB_USER')),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', os.getenv('DB_PASSWORD')),
-        'HOST': os.environ.get('POSTGRES_HOST', os.getenv('DB_HOST')),
-        'PORT': os.environ.get('POSTGRES_PORT', os.getenv('DB_PORT')),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
